4cfc174c8c91e3ee1de4e1fab503ab3f086bca72
Maxime Huran <m.huran@monsieurbiz.com>
2020-03-24 16:43:05 +0100

    Add admin menu item for CMS and page

---
+++ src/Listener/AdminMenuListener.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace MonsieurBiz\SyliusCmsPagePlugin\Listener;
+
+use Sylius\Bundle\UiBundle\Menu\Event\MenuBuilderEvent;
+
+final class AdminMenuListener
+{
+
+    public function addAdminMenuItem(MenuBuilderEvent $event): void
+    {
+        $menu = $event->getMenu();
+
+        $content = $menu
+            ->addChild('mbiz-cms')
+            ->setLabel('monsieurbiz_cms_page.ui.cms_content')
+        ;
+
+        $content->addChild('mbiz-cms-page', ['route' => 'monsieurbiz_cms_page_admin_page_index'])
+            ->setLabel('monsieurbiz_cms_page.ui.pages')
+            ->setLabelAttribute('icon', 'file alternate')
+        ;
+    }
+
+}
