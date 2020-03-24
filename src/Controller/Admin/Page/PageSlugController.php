60cabf7c18d3daac4b41f75b59de24575cba96bb
Maxime Huran <m.huran@monsieurbiz.com>
2020-03-24 19:40:51 +0100

    Add controller to generate a slug

---
+++ src/Controller/Admin/Page/PageSlugController.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MonsieurBiz\SyliusCmsPagePlugin\Controller\Admin\Page;
+
+use Sylius\Component\Product\Generator\SlugGeneratorInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+class PageSlugController
+{
+    /** @var SlugGeneratorInterface */
+    private $slugGenerator;
+
+    /**
+     * RecipeSlugController constructor.
+     * @param SlugGeneratorInterface $slugGenerator
+     */
+    public function __construct(SlugGeneratorInterface $slugGenerator)
+    {
+        $this->slugGenerator = $slugGenerator;
+    }
+
+    /**
+     * Generate slug and return JSON
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function generateAction(Request $request): JsonResponse
+    {
+        $name = $request->query->get('title');
+
+        return new JsonResponse([
+            'slug' => $this->slugGenerator->generate($name),
+        ]);
+    }
+}
