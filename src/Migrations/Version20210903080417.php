33a3ad6c151b2a06dca6d08be37ab793de13c1b9
Matteo <matteo.ftt@gmail.com>
2021-09-03 10:56:56 +0200

    Adding license bloc

--- src/Migrations/Version20210903080417.php
+++ src/Migrations/Version20210903080417.php
@@ -1,8 +1,17 @@
 <?php
 
+/*
+ * This file is part of Monsieur Biz' Cms Page plugin for Sylius.
+ *
+ * (c) Monsieur Biz <sylius@monsieurbiz.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 declare(strict_types=1);
 
-namespace App\Migrations;
+namespace MonsieurBiz\SyliusCmsPagePlugin\Migrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
