faf833eaeb180347fdfc5b188854bba446cf79d5
Maxime Huran <m.huran@monsieurbiz.com>
2021-12-22 16:30:45 +0100

    Use custom PageController for page resource with a previewAction

---
+++ src/Controller/PageController.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of Monsieur Biz' Cms Page plugin for Sylius.
+ *
+ * (c) Monsieur Biz <sylius@monsieurbiz.com>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace MonsieurBiz\SyliusCmsPagePlugin\Controller;
+
+use Sylius\Bundle\ResourceBundle\Controller\ResourceController;
+use Sylius\Component\Resource\ResourceActions;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class PageController extends ResourceController
+{
+    public function previewAction(Request $request): Response
+    {
+        $configuration = $this->requestConfigurationFactory->create($this->metadata, $request);
+
+        $this->isGrantedOr403($configuration, ResourceActions::SHOW);
+        $resource = $this->findOr404($configuration);
+
+        return $this->render($configuration->getRequest()->get('template'), [
+            'configuration' => $configuration,
+            'metadata' => $this->metadata,
+            'resource' => $resource,
+            $this->metadata->getName() => $resource,
+        ]);
+    }
+}
